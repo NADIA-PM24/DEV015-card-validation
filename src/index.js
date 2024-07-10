588c034427e8b9f1ddd7a3095718667e4483c2e6
NADIA-PM24 <nadperezmendez@gmail.com>
2024-07-10 11:37:12 -0600

    Este es mi 1er commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import validator from './validator.js';
 
+let numeros = [4, 2, 4, 2, 4, 2, 4, 2, 4, 2, 4, 2, 4, 2, 4, 2,];
+numeros.reverse();
+console.log(numeros); 
+
 console.log(validator);
+
+const validatorCard = {
+
+//const númeroDetarjeta = 4242424242424242;
+//colocar el número para hacer el reverso de la tarjeta
+//número de tarjeta reverso
+//transformar desde un string a un array
+//RECORRER ese nuevo array
+//después de esto debo de recorrer las posiciones pares
+//multiplicar los números pares
+//después de números pares debe aparecer que sea una tarjeta válida
+//
+} 
+console.log(validatorCard);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,15 @@
-const validator = {
-  // ...
+const validator = require ('4242 4242 4242 4242'){
+  letCardInformation = '4242 4242 4242 4242';
+  
+  if (validator.isCardInformation(4242424242424242)){
+    console.log('El número de la tarjeta es válido.');
+} 
+  else {
+    console.log('El número de la tarjeta no es válido.');
 };
+
+
+
+
 
 export default validator;
